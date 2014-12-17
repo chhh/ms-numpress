de8f5c2af6e3f7b542fa524152ef0c2ee36390c5
Dmitry Avtonomov <dmitriy.avtonomov@gmail.com>
2014-12-16 21:07:05 -0500

    Javadoc + .gitignore + reformatting

--- src/main/java/ms/numpress/IntDecoder.java
+++ src/main/java/ms/numpress/IntDecoder.java
@@ -16,61 +16,61 @@
    See the License for the specific language governing permissions and
    limitations under the License.
  */
- package ms.numpress;
+package ms.numpress;
 
 /**
- * Decodes ints from the half bytes in bytes. Lossless reverse of encodeInt, 
- * although not symmetrical in input arguments. 
+ * Decodes ints from the half bytes in bytes. Lossless reverse of encodeInt,
+ * although not symmetrical in input arguments.
  */
 class IntDecoder {
-	
-	int pos = 0;
-	boolean half = false;
-	byte[] bytes;
-	
-	public IntDecoder(byte[] _bytes, int _pos) {
-		bytes 	= _bytes;
-		pos 	= _pos;
-	}
-	
-	public long next() {
-		int head;
-		int i, n;
-		long res = 0;
-		long mask, m;
-		int hb;
-		
-		if (!half)
-			head = (0xff & bytes[pos]) >> 4;
-		else
-			head = 0xf & bytes[pos++];
 
-		half = !half;
-		
-		if (head <= 8)
-			n = head;
-		else { 
-			// leading ones, fill in res
-			n = head - 8;
-			mask = 0xf0000000;
-			for (i=0; i<n; i++) {
-				m = mask >> (4*i);
-				res = res | m;
-			}
-		}
+    int pos = 0;
+    boolean half = false;
+    byte[] bytes;
 
-		if (n == 8) return 0;
-			
-		for (i=n; i<8; i++) {
-			if (!half)
-				hb = (0xff & bytes[pos]) >> 4;
-			else
-				hb = 0xf & bytes[pos++];
+    public IntDecoder(byte[] _bytes, int _pos) {
+        bytes = _bytes;
+        pos = _pos;
+    }
 
-			res = res | (hb << ((i-n)*4));
-			half = !half;
-		}
-		
-		return res;
-	}
+    public long next() {
+        int head;
+        int i, n;
+        long res = 0;
+        long mask, m;
+        int hb;
+
+        if (!half)
+            head = (0xff & bytes[pos]) >> 4;
+        else
+            head = 0xf & bytes[pos++];
+
+        half = !half;
+
+        if (head <= 8)
+            n = head;
+        else {
+            // leading ones, fill in res
+            n = head - 8;
+            mask = 0xf0000000;
+            for (i = 0; i < n; i++) {
+                m = mask >> (4 * i);
+                res = res | m;
+            }
+        }
+
+        if (n == 8) return 0;
+
+        for (i = n; i < 8; i++) {
+            if (!half)
+                hb = (0xff & bytes[pos]) >> 4;
+            else
+                hb = 0xf & bytes[pos++];
+
+            res = res | (hb << ((i - n) * 4));
+            half = !half;
+        }
+
+        return res;
+    }
 }
